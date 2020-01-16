153ef3d564957d5da2972331dc0d9b052ffc20dd
FinnPegler <finnpegler@gmail.com>
2020-01-16 22:57:01 +0000

    changes to copy

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var iframe = document.getElementById("booking-widget-iframe");
-var field = iframe.contentWindow.document.querySelector('booking[email]');
+var field = iframe.contentWindow.document.querySelector("booking[email]");
 
 function changeCopy() {
 field.placeholder = "hello";
-//console.log(field)
 }
 
 document.getElementById("button").addEventListener("click", changeCopy)
