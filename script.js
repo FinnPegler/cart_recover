17d1c11dacce35e8210ff15f1672401fa1bd17f6
FinnPegler <finnpegler@gmail.com>
2020-01-18 02:27:19 +0000

    changed regex

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let str = "";
 
 function changeCopy(event) {
 str = (field.value)
-if (str.match(/.com/i)) {console.log("string is: " + str)}
+if (str.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i)) {console.log("Email is: " + str)}
 }
 
 field.addEventListener("keyup", changeCopy)
